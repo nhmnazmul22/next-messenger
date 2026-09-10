8fb9b07c3e35a2ac57be3e928659e4879cef31e7
Nhm Nazmul <najmulmonir12345@gmail.com>
2026-09-10 23:04:11 +0600

    feat: update avatarUrl field type to text in users table and adjust validation rules in RegisterRequest

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('avatarUrl')->nullable();
+            $table->text('avatarUrl')->nullable();
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
