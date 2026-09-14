ed91e1d7796d566bfb6d7032ae7c8ce260a3488f
Nhm Nazmul <najmulmonir12345@gmail.com>
2026-09-14 13:01:58 +0600

    feat: update DatabaseSeeder to include additional user data for seeding

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,71 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = [
+            [
+                'name' => 'Arif Rahman',
+                'email' => 'arif@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=1',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Nusrat Jahan',
+                'email' => 'nusrat@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=2',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Sakib Hasan',
+                'email' => 'sakib@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=3',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Mim Akter',
+                'email' => 'mim@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=4',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Tanvir Ahmed',
+                'email' => 'tanvir@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=5',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Sadia Islam',
+                'email' => 'sadia@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=6',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Fahim Chowdhury',
+                'email' => 'fahim@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=7',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Jannatul Ferdous',
+                'email' => 'jannatul@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=8',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Rafi Ahmed',
+                'email' => 'rafi@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=9',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Sumaiya Rahman',
+                'email' => 'sumaiya@example.com',
+                'avatarUrl' => 'https://i.pravatar.cc/150?img=10',
+                'password' => Hash::make('password'),
+            ],
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($users as $user) {
+            User::factory()->create($user);
+        }
     }
 }
